2c44776eacbd7f9b57ac4ade6eb86872e3d3c42a
Node1 <krejov100@gmail.com>
2018-11-26 16:20:21 -0800

    Client changes

--- EtherSense.py
+++ EtherSense.py
@@ -84,6 +84,74 @@ class AsyncoreMulticastServer(asyncore.dispatcher):
         print('received %s bytes from %s' % (data, addr))
 
 
+def getDepthAndColorMat(pipeline):
+    frames = rs.composite_frame(rs.frame())
+    frames = pipeline.poll_for_frames()
+    if frames.size() > 0:
+        depth = frames.get_depth_frame()
+        depthData = depth.as_frame().get_data()
+        depthMat = np.asanyarray(depthData)
+        return depthMat
+
+def openBagPipeline(filename):
+    cfg = rs.config()
+    cfg.enable_device_from_file(filename)
+    pipeline = rs.pipeline()
+    pipeline.start(cfg)
+    return pipeline
+
+
+class RealSenseUDPserver(asyncore.dispatcher):
+    import pyrealsense2 as rs
+    def __init__(self, address):
+        asyncore.dispatcher.__init__(self)
+        print("Launching Realsense Camera Server")
+        self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print('sending acknowledgement to', address)
+        self.connect(address)
+        self.socket.send('ack'.encode())
+        self.pipeline = openBagPipeline("/home/node1/Desktop/20181119_131946.bag")
+        self.ready = True
+
+    def writable(self):
+        return hasattr(self,'pipeline')
+    
+
+    def handle_write(self):
+        depth = getDepthAndColorMat(self.pipeline)
+        if depth is not None:
+            smallDepth = cv2.resize(depth, (0,0), fx=0.15, fy=0.15) 
+            data = pickle.dumps(smallDepth)
+            print(len(data))
+            while data:
+                data = data[self.socket.send(data):]        
+
+
+class AsyncoreMulticastServer(asyncore.dispatcher):
+    def __init__(self, host = mc_ip_address, port=1024):
+        asyncore.dispatcher.__init__(self)
+        server_address = ('', port)
+        self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
+        group = socket.inet_aton(host)
+        mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.bind(server_address)
+ 	
+    def handle_read(self):
+        data, addr = self.socket.recvfrom(42)
+        print('received %s bytes from %s' % (data, addr))
+        RealSenseUDPserver(addr)
+        print(sys.stderr, data)
+
+    def writable(self): 
+        return False # don't want write notifies
+
+    def handle_accept(self):
+        channel, addr = self.accept()
+        print('received %s bytes from %s' % (data, addr))
+
+
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hs", ["is_server="])
@@ -147,11 +215,38 @@ def wait_for_multi_cast(ip_address, port):
         print('sending acknowledgement to', address)
         sock.sendto('ack', address)
 
+class RealSenseUDPclient(asyncore.dispatcher):
+
+    def __init__(self, address):
+        asyncore.dispatcher.__init__(self)
+        print("Launching Realsense Camera Client")
+        self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.sock.settimeout(0.5)
+        print('sending acknowledgement to', address)
+        self.connect(address)
+        #self.socket.send('ack'.encode())
+        self.pipeline = openBagPipeline("/home/node1/Desktop/20181119_131946.bag")
+        self.ready = True
+
+    def readable(self):
+        return True
+    
+
+    def handle_read(self):
+        data, server = sock.recvfrom(65507)
+        ts, imdata = data[:struct.calcsize('d')], data[struct.calcsize('d'):]
+        timestamp = struct.unpack('d', ts)
+        imdata = pickle.loads(imdata)
+        bigDepth = cv2.resize(imdata, (0,0), fx=2, fy=2, interpolation=cv2.INTER_NEAREST) 
+        cv2.putText(bigDepth, str(timestamp), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (65536), 2, cv2.LINE_AA)
+        cv2.imshow(str(server[1]), bigDepth)
+        cv2.waitKey(1)
+
 def multi_cast_message(ip_address, port, message):
     multicast_group = (ip_address, port)
     #send out regular request for servers on network
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(0.2)
+    sock.settimeout(0.5)
     # Set the time-to-live for messages to 1 so they do not go past the
     # local network segment.
     ttl = struct.pack('b', 1)
@@ -161,19 +256,21 @@ def multi_cast_message(ip_address, port, message):
         # Send data to the multicast group
         print('sending "%s"' % message + str(multicast_group))
         sent = sock.sendto(message.encode(), multicast_group)
-
+        
         # Look for responses from all recipients
         while True:
-            print('waiting to receive')
             try:
-                data, server = sock.recvfrom(42)
+                data, server = sock.recvfrom(65507)
+                ts, imdata = data[:struct.calcsize('d')], data[struct.calcsize('d'):]
+                timestamp = struct.unpack('d', ts)
+                imdata = pickle.loads(imdata)
+                bigDepth = cv2.resize(imdata, (0,0), fx=2, fy=2, interpolation=cv2.INTER_NEAREST) 
+                cv2.putText(bigDepth, str(timestamp), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (65536), 2, cv2.LINE_AA)
+                cv2.imshow(str(server[1]), bigDepth)
+                cv2.waitKey(1)
             except socket.timeout:
                 print('timed out, no more responses')
                 break
-            else:
-                print('received "%s" from %s' % (data, server))
-                
-
     finally:
         print(sys.stderr, 'closing socket')
         sock.close()
