963b94ccaf0ed53699f79062800e1ef4d79518f2
Philip Krejov <philip.krejov@intel.com>
2019-01-22 14:59:48 -0800

    explicity requires python of EtherSenseServer

--- AlwaysRunningServer.bash
+++ AlwaysRunningServer.bash
@@ -1,6 +1,6 @@
-#!/bin/bash
+"#!/bin/bash
 # this statement checks if there is an instance of the EtherSenseServer running
-if [[ ! `ps -eaf | grep EtherSenseServer.py | grep -v grep` ]]; then
+if [[ ! `ps -eaf | grep "python EtherSenseServer.py" | grep -v grep` ]]; then
 # if not, EtherSenseServer is started with the PYTHONPATH set due to cron not passing Env 
     PYTHONPATH=$HOME/.local/lib/python2.7/site-packages python EtherSenseServer.py
 fi
