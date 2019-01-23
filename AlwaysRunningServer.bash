5ec2923564fef749455b7169e2e073ba61a70dc2
Philip Krejov <philip.krejov@intel.com>
2019-01-23 11:05:48 -0800

    removed error quote

--- AlwaysRunningServer.bash
+++ AlwaysRunningServer.bash
@@ -1,4 +1,4 @@
-"#!/bin/bash
+#!/bin/bash
 # this statement checks if there is an instance of the EtherSenseServer running
 if [[ ! `ps -eaf | grep "python EtherSenseServer.py" | grep -v grep` ]]; then
 # if not, EtherSenseServer is started with the PYTHONPATH set due to cron not passing Env 
