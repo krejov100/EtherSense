5ac84a098e6e4ec3db0bd38d12590d4827305e58
Node1 <krejov100@gmail.com>
2019-01-29 14:46:53 -0800

    remove redundent files

--- cron_setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-
-# https://stackoverflow.com/questions/2366693/run-cron-job-only-if-it-isnt-already-running
-# http://blog.appliedinformaticsinc.com/managing-cron-jobs-with-python-crontab/
-
-
-from crontab import CronTab
