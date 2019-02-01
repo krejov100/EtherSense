49fe867fc77341ee62bfcd8ec83c3fe040011210
Philip Krejov <philip.krejov@intel.com>
2019-01-31 16:09:01 -0800

    remove decimate filter

--- EtherSenseServer.py
+++ EtherSenseServer.py
@@ -22,11 +22,11 @@ def getDepthAndTimestamp(pipeline, depth_filter):
     frames.keep()
     depth = frames.get_depth_frame()
     if depth:
-	depth2 = depth_filter.process(depth)
+	#depth2 = depth_filter.process(depth)
 	# take owner ship of the frame for further processing
-	depth2.keep()
+	depth.keep()
 	# represent the frame as a numpy array
-        depthData = depth2.as_frame().get_data()        
+        depthData = depth.as_frame().get_data()        
 	depthMat = np.asanyarray(depthData)
 	ts = frames.get_timestamp()
         return depthMat, ts
