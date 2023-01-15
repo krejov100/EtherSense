33eab044043667f0118e4db1c7c46112c3a6fadd
Baek Donghyeon <nistring@naver.com>
2023-01-15 14:47:04 +0900

    bug fixed

--- EtherSenseClient.py
+++ EtherSenseClient.py
@@ -8,26 +8,76 @@ import socket
 import struct
 import cv2
 
-
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
 mc_ip_address = '224.0.0.1'
-local_ip_address = '192.168.0.1'
+depth_scale = 0.0010000000474974513
 port = 1024
 chunk_size = 4096
 
+max_distance = 3.0 # m
+min_distance = 0.3 # m
+
 def main(argv):
     multi_cast_message(mc_ip_address, port, 'EtherSensePing')
-        
+
+def D2RGB(depth_map):
+    # Convert depth map to float type
+    d = depth_map.astype(np.float)
+
+    # Initialize empty RGB image
+    rgb = np.zeros((d.shape[0], d.shape[1], 3), dtype=np.uint8)
+
+    # Compute RGB channels from depth map
+    r = np.zeros_like(d)
+    g = np.zeros_like(d)
+    b = np.zeros_like(d)
+
+    # Normalize depth
+    d_normal = 1529.0 * (d - min_distance) / (max_distance - min_distance)
+    d_normal = np.rint(d_normal)
+
+    # D2R
+    r[np.logical_or(d_normal < 255, 1275 <= d_normal)] = 255
+
+    condition = np.logical_and(255 <= d_normal, d_normal < 510)
+    r[condition] = (509 - d_normal)[condition]
+
+    condition = np.logical_and(1020 <= d_normal, d_normal < 1275)
+    r[condition] = (d_normal - 1020)[condition]
+
+    # D2G
+    condition = d_normal < 255
+    g[condition] = d_normal[condition]
+
+    g[np.logical_and(255 <= d_normal, d_normal < 765)] = 255
+
+    condition = np.logical_and(765 <= d_normal, d_normal < 1020)
+    g[condition] = (1019 - d_normal)[condition]
+
+    # D2B
+    condition = np.logical_and(510 <= d_normal, d_normal < 765)
+    b[condition] = (d_normal - 510)[condition]
+
+    b[np.logical_and(765 <= d_normal, d_normal < 1275)] = 255
+
+    condition = 1275 <= d_normal
+    b[condition] = (1529 - d_normal)[condition]
+
+    # Set RGB channels to RGB image
+    rgb[:,:,0] = r
+    rgb[:,:,1] = g
+    rgb[:,:,2] = b
+
+    return rgb
+
 
 #UDP client for each camera server 
 class ImageClient(asyncore.dispatcher):
     def __init__(self, server, source):   
         asyncore.dispatcher.__init__(self, server)
-        self.address = server.getsockname()[0]
-        self.port = source[1]
         self.buffer = bytearray()
-        self.windowName = self.port
+        self.windowName = source[1]
         # open cv window which is unique to the port 
         cv2.namedWindow("window"+str(self.windowName))
         self.remainingBytes = 0
@@ -52,9 +102,10 @@ class ImageClient(asyncore.dispatcher):
     def handle_frame(self):
         # convert the frame from string to numerical data
         imdata = pickle.loads(self.buffer)
-        bigDepth = cv2.resize(imdata, (0,0), fx=2, fy=2, interpolation=cv2.INTER_NEAREST) 
+        bigDepth = cv2.resize(imdata, (0,0), fx=2, fy=2, interpolation=cv2.INTER_NEAREST)
+        bigDepth = D2RGB(bigDepth * depth_scale)
         cv2.putText(bigDepth, str(self.timestamp), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (65536), 2, cv2.LINE_AA)
-        cv2.imshow("window"+str(self.windowName), bigDepth)
+        cv2.imshow("window"+str(self.windowName), cv2.cvtColor(bigDepth, cv2.COLOR_RGB2BGR))
         cv2.waitKey(1)
         self.buffer = bytearray()
         self.frame_id += 1
@@ -65,7 +116,7 @@ class ImageClient(asyncore.dispatcher):
 class EtherSenseClient(asyncore.dispatcher):
     def __init__(self):
         asyncore.dispatcher.__init__(self)
-        self.server_address = ('', 1024)
+        self.server_address = ('', port)
         # create a socket for TCP connection between the client and server
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.settimeout(5)
@@ -82,11 +133,8 @@ class EtherSenseClient(asyncore.dispatcher):
     def handle_connect(self):
         print("connection recvied")
 
-    def handle_accept(self):
-        pair = self.accept()
-        #print(self.recv(10))
-        if pair is not None:
-            sock, addr = pair
+    def handle_accepted(self, sock, addr):
+        if sock is not None:
             print ('Incoming connection from %s' % repr(addr))
             # when a connection is attempted, delegate image receival to the ImageClient 
             handler = ImageClient(sock, addr)
@@ -95,14 +143,17 @@ def multi_cast_message(ip_address, port, message):
     # send the multicast message
     multicast_group = (ip_address, port)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    connections = {}
+    # Limit multicasting to local network.
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+    
     try:
+        client = EtherSenseClient()
         # Send data to the multicast group
         print('sending "%s"' % message + str(multicast_group))
-        sent = sock.sendto(message.encode(), multicast_group)
+        sock.sendto(message.encode(), multicast_group)
    
         # defer waiting for a response using Asyncore
-        client = EtherSenseClient()
+        
         asyncore.loop()
 
         # Look for responses from all recipients
